2f5d851f40640fed87dcbb7ce3297b51629f528c
arleishas <148395284+arleishas@users.noreply.github.com>
2023-11-01 17:48:40 -0700

    Homework 20
    Added Explicit Waits to Homework 19

---
+++ src/test/java/Homework20.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework20 extends BaseTest{
+
+    @Test
+    public void deletePlaylist() throws InterruptedException {
+        String expectedDeletedPlaylistSuccessMsg = "Deleted playlist \"AutoTestL19.\"";
+        navigateToLoginPage();
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        clickSubmit();
+        openPlaylist();
+        clickDeletePlaylistBtn();
+        Assert.assertEquals(getDeletedPlaylistSuccessMsg(), expectedDeletedPlaylistSuccessMsg);
+
+    }
+    public void openPlaylist() throws InterruptedException{
+        WebElement emptyPlaylist = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#playlists > ul > li:nth-child(5) > a")));
+        //WebElement emptyPlaylist = driver.findElement(By.cssSelector("#playlists > ul > li:nth-child(5) > a"));
+        emptyPlaylist.click();
+
+    }
+    public void clickDeletePlaylistBtn() throws InterruptedException {
+        WebElement deletePlaylistBtn = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".btn-delete-playlist")));
+        //WebElement deletePlaylistBtn = driver.findElement(By.cssSelector(".btn-delete-playlist"));
+        deletePlaylistBtn.click();
+    }
+
+    public String getDeletedPlaylistSuccessMsg(){
+        WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+        //WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
+        return notification.getText();
+    }
+}
+
